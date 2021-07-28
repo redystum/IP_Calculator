c412011127f6ed7dd804bc286a7bf9351aac0765
redystum <63860553+redystum@users.noreply.github.com>
2021-07-28 20:32:57 +0100

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import re
 from ipaddress import IPv4Address, IPv4Network
+import ipaddress
 
 root = Tk()
 
@@ -12,6 +13,7 @@ root.title("IP Calcualtor")
 
 root.configure(bg='#2e2e2e')
 
+# Validate if imput its a valid IP and Mask
 def validate():
     ip = ip_input.get()
     mask = mask_input.get()
@@ -45,7 +47,7 @@ def validate():
             validate_var.set("Enter a valid IP")
             ip_input.delete(0, END)
 
-
+# Binary IP
 def binar():
     num = ip_input.get().split(".")
     i = 0
@@ -62,7 +64,62 @@ def binar():
     ip_binar = f"{n[0]}.{n[1]}.{n[2]}.{n[3]}"
     return ip_binar
 
+# Network
+def network():
+    ip = f"{ip_input.get()}/{int(mask_input.get())}"
+    network_num = ipaddress.ip_network(ip, strict=False)
+    return network_num
 
+# Network
+def network_bin(network_num):
+    network_num = str(network_num)
+    net = network_num.split("/")
+    network = net[0].split(".")
+    i = 0
+    n = ["", "", "", ""]    
+    for i in range(0, 4):
+        n[i] = bin(int(network[i]))[2:]
+        i = i + 1
+
+    network_binar = f"{n[0]}.{n[1]}.{n[2]}.{n[3]}"
+    return network_binar
+
+# Mask
+def Mask():
+    i = 0
+    netmask_bin = StringVar()
+    num_mask_string = StringVar()
+    num_mask = ["0","0","0","0","0","0","0","0",".","0","0","0","0","0","0","0","0",".","0","0","0","0","0","0","0","0",".","0","0","0","0","0","0","0","0"]
+    one_number = int(mask_input.get())
+    if one_number > 24 :
+        one_number = one_number + 3
+    elif one_number > 16 :
+        one_number = one_number + 2
+    elif one_number > 8 :
+        one_number = one_number + 1
+    for i in range(0, one_number):
+        if (i == 8 or i == 17 or i == 26):
+            num_mask[i] = "."
+        else:
+            num_mask[i] = "1"
+        i = i + 1
+    netmask_bin = str(num_mask)
+    netmask_bin = netmask_bin.replace(" ","").replace("'","").replace(",","").replace("[","").replace("]","")
+    return netmask_bin
+
+# Mask
+def mask_bin_covertion(netmask_bin):
+    num = netmask_bin.split(".")
+    i = 0
+    n = ["", "", "", ""]
+    i = i + 1
+    for i in range(0, 4):
+        n[i] = int(num[i], 2)
+        i = i + 1
+    netmask = f"{n[0]}.{n[1]}.{n[2]}.{n[3]}"
+    return netmask
+
+# Class
 def class_ip(n):
     classA = IPv4Network(("10.0.0.0", "255.0.0.0"))
     classB = IPv4Network(("172.16.0.0", "255.240.0.0"))
@@ -89,64 +146,132 @@ def class_ip(n):
 
     return ip_class
 
+# Broadcast
+def broadcast_calc():
+    ip = f"{ip_input.get()}/{int(mask_input.get())}"
+    net = ipaddress.IPv4Network(ip, False )
+    broadcast = net.broadcast_address
+    return broadcast
 
-def Mask():
-    i = 0
-    netmask_bin = StringVar()
-    num_mask_string = StringVar()
-    num_mask = ["0","0","0","0","0","0","0","0",".","0","0","0","0","0","0","0","0",".","0","0","0","0","0","0","0","0",".","0","0","0","0","0","0","0","0"]
-    one_number = int(mask_input.get())
-    for i in range(0, one_number):
-        if (i == 8 or i == 16 or i == 24):
-            num_mask[i] = "."
-        else:
-            num_mask[i] = "1"
-        i = i + 1
-    netmask_bin = str(num_mask)
-    return netmask_bin
-
-def mask_bin_covertion(netmask_bin):
-    num = netmask_bin.split(".")
+# Broadcast
+def broadcast_bin(broadcast):
+    broadcast = str(broadcast)
+    num = broadcast.split(".")
     i = 0
     n = ["", "", "", ""]
     i = i + 1
-    i = 0
     for i in range(0, 4):
-        n[i] = int(num[i], 2)
+        n[i] = bin(int(num[i]))[2:]
         i = i + 1
-    netmask = f"{n[0]}.{n[1]}.{n[2]}.{n[3]}"
-    return netmask
+    broadcast_binar = f"{n[0]}.{n[1]}.{n[2]}.{n[3]}"
+    return broadcast_binar
+
+# First Host
+def fist_host(network_num):
+    network_num = str(network_num)
+    network_num = network_num.split("/")
+    network_num = network_num[0].split(".")
+    if network_num[3] == "255":
+        first = network_num
+    else:
+        network_num[3] = int(network_num[3])
+        network_num[3] = network_num[3] + 1
+        first = network_num
+    first = str(first)
+    first = first.replace(" ","").replace("'","").replace(",",".").replace("[","").replace("]","")
+    return first
+
+# First on Host
+def first_bin(first):
+    num = first.split(".")
+    i = 0
+    n = ["", "", "", ""]
+    i = i + 1
+    for i in range(0, 4):
+        n[i] = bin(int(num[i]))[2:]
+        i = i + 1
+    first_binar = f"{n[0]}.{n[1]}.{n[2]}.{n[3]}"
+    return first_binar
+
+# Last on Host
+def last_host(broadcast):
+    broadcast = str(broadcast)
+    broadcast = broadcast.split("/")
+    broadcast = broadcast[0].split(".")
+    broadcast[3] = int(broadcast[3])
+    broadcast[3] = broadcast[3] - 1 # THIS LINE DONT WORK, IDK WHY
+    last = broadcast
+    last = str(last)
+    last = last.replace(" ","").replace("'","").replace(",",".").replace("[","").replace("]","")
+    return last
+
+# Last on Host
+def last_bin(last):
+    num = last.split(".")
+    i = 0
+    n = ["", "", "", ""]
+    i = i + 1
+    for i in range(0, 4):
+        n[i] = bin(int(num[i]))[2:]
+        i = i + 1
+    last_binar = f"{n[0]}.{n[1]}.{n[2]}.{n[3]}"
+    return last_binar
 
 
+# Hosts
+def hosts_number():
+    mask = int(mask_input.get())
+    if mask == 32:
+        hosts = 1
+    else:
+        zero_number = 32 - mask
+        hosts = (2**zero_number)-2
+    return hosts
 
-
-
+# Display results
 def screen_out():
     # Binary IP
     ip_binar = binar()
     ip_out.set(ip_input.get())
     ip_binary_out.set(ip_binar)
+
     # Network
-    
+    network_num = network()
+    network_out.set(network_num)
+    network_binar = network_bin(network_num)
+    network_binary_out.set(network_binar)
+
     # Mask
     netmask_bin = Mask()
-    netmask_bin = netmask_bin.replace(" ","").replace("'","").replace(",","").replace("[","").replace("]","")
     mask_binary_out.set(netmask_bin)
     netmask = mask_bin_covertion(netmask_bin)
     mask_out.set(netmask)
+
     # class
     ip_class = class_ip(ip_binar)
     class_out.set(ip_class)
+
     # Broadcast
+    broadcast = broadcast_calc()
+    broadcast_out.set(broadcast)
+    broadcast_binar = broadcast_bin(broadcast)
+    broadcast_binary_out.set(broadcast_binar)
 
-    # First Host
+    # First on Host
+    first = fist_host(network_num)
+    first_out.set(first)
+    first_binar = first_bin(first)
+    first_binary_out.set(first_binar)
 
-    # last host
+    # last on host
+    last = fist_host(broadcast)
+    last_out.set(last)
+    last_binar = first_bin(last)
+    last_binary_out.set(last_binar)
 
     # Hosts
-
-    1+1
-
+    hosts = hosts_number()
+    hosts_out.set(hosts)
 
 # Main
 
